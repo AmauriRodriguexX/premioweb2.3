b7527308122611847421231df8584888ba891e56
AmauriRodriguexX <amauri.rodriguez@t2o.com>
2025-06-24 14:47:55 -0600

    cambios

--- js/form-step-upload.js
+++ js/form-step-upload.js
@@ -197,8 +197,9 @@ function manejarCargaArchivos() {
             if (!checkboxAcepto.checked) {
                 errorCheckboxMsg.textContent = 'Debes aceptar el uso de tu imagen.';
                 formValido = false;
+            }else{
+                localStorage.setItem('aviso', "si");
             }
-
         });
     }
 }
